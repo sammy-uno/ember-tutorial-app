291d14def130c40fd235cd1aef8d12f683266cc6
sammy-uno <mustaphafriha@gmail.com>
2020-12-18 02:28:53 -0600

    EmberJs Labs

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoic2FtMjAyMiIsImEiOiJja2lubjdzaTkwa20zMnNud3kxdzRtenkwIn0.TD-gRrRTJAhAMm9Y4N-qZQ"
+	  
   return ENV;
 };
